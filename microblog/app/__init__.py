6ffe5f8ab75eda7a88e0f8989e7ea1871220563b
Shh0718 <78247302+Shh0718@users.noreply.github.com>
2021-03-05 14:51:31 +0800

    Update __init__.py

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask#从flask包中导入Flask类
+from flask import Flask  # 从flask包中导入Flask类
+from config import Config
 
-app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+from flask_sqlalchemy import SQLAlchemy  # 从包中导入类
+from flask_migrate import Migrate
 
-#print('等会谁（哪个包或模块）在使用我：',__name__)
+app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+app.config.from_object(Config)
 
-from app import routes#从app包中导入模块routes
+db = SQLAlchemy(app)  # 数据库对象
+migrate = Migrate(app, db)  # 迁移引擎对象
+# print('等会谁（哪个包或模块）在使用我：',__name__)
 
-#注：上面两个app是完全不同的东西。两者都是纯粹约定俗成的命名，可重命名其他内容。
+from app import routes  # 从app包中导入模块routes
+
+# 注：上面两个app是完全不同的东西。两者都是纯粹约定俗成的命名，可重命名其他内容。
